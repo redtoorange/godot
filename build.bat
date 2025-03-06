f09b3010c8e9b2dd9266c976a6e6fe526cdf1f87
Roy Berardo <RoyBerardo@users.noreply.github.com>
2025-03-06 15:57:48 -0800

    Change AudioStreamRandomizer random_pitch slider to random_pitch_semitones
    Create test class
    
    Start working on avoidance system
    
    Added repulsion node
    
    Update to enable build profile
    
    Cleaned up module
    
    update build config
    
    Update build files

---
+++ build.bat
@@ -0,0 +1,1 @@
+call scons progress=no platform=windows target=editor vsproj=yes arch=x86_64 fast_unsafe=yes
---
+++ build_exports.bat
@@ -0,0 +1,2 @@
+call scons progress=no platform=windows target=template_release arch=x86_64 fast_unsafe=yes
+call scons progress=no platform=windows target=template_debug arch=x86_64 fast_unsafe=yes
