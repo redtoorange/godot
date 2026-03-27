e7cd219b61664281012a443e79ef24990e9024ef
Andrew McGuiness <redtoorange@gmail.com>
2026-03-27 05:12:33 -0400

    Build files

---
+++ build.bat
@@ -0,0 +1,1 @@
+call scons progress=no accesskit=no platform=windows target=editor vsproj=yes arch=x86_64 fast_unsafe=yes
---
+++ build_net.bat
@@ -0,0 +1,9 @@
+@rem Build Binaries
+call scons platform=windows target=editor accesskit=no module_mono_enabled=yes fast_unsafe=yes d3d12=yes scu_build=yes scu_limit=1024 build_profile="gem_knights_profile.gdbuild"
+
+@rem Generate glue sources
+call bin/godot.windows.editor.x86_64.mono.exe --headless --generate-mono-glue modules/mono/glue
+
+@rem Build .NET assemblies
+call python ./modules/mono/build_scripts/build_assemblies.py --godot-output-dir=./bin --godot-platform=windows --push-nupkgs-local=D:\Workspace\MyLocalNugetSource
+
